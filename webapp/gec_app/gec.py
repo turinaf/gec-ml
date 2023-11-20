24361644574f8b98ed9dd0f9f2bb01694379f3c6
Turi <40395348+turinaf@users.noreply.github.com>
2023-11-20 22:10:40 +0800

    Update with comment
    Added some comments to gec_app.py

--- webapp/gec_app/gec.py
+++ webapp/gec_app/gec.py
@@ -7,17 +7,20 @@ corrector = pipeline("text2text-generation", model="../server/gec-t5-base-40ep")
 @bp.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # get input from the form in templates/index.html
         input_text = request.form.get("input_text")
-        sentences = input_text.split(".")
+        sentences = input_text.split(".") # separate user input into several sentences using period as delimiter
         corrections = []
         annotated_input = []
         for i in range(len(sentences)):
             if len(sentences[i]) == 0:
                 continue
+            # pass sentences
             corrected = corrector(f"grammar: {sentences[i]}")
             corrected_text = corrected[0]["generated_text"]
             
             # add span with class for all changed string
+            # compare original sentences and model correction and add span where there's a change in the sentences
             annotated_sentence = ""
             anno_input = ""
             cor_split = corrected_text.split(" ")
@@ -35,7 +38,9 @@ def index():
                     anno_input = anno_input + word + " "
             annotated_input.append(anno_input)   
             corrections.append(annotated_sentence)
+            # return the results in json format
         return jsonify({"corrections": corrections, "annotated_input": annotated_input})
 
     else:
+        # if there not request for correction, just render the form
         return render_template("index.html", input_text=None, corrections=None)
